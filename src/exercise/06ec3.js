50492fb9bb8293fa6afc701431510a1ff3a36f1e
TomGower <tom.gower@gmail.com>
2021-02-01 13:29:58 -0600

    Solution for exercise 06 extra credit 3

--- src/exercise/06ec3.js
+++ src/exercise/06ec3.js
@@ -105,6 +105,8 @@ function Grid() {
 }
 Grid = React.memo(Grid)
 
+// Higher Order Component
+// this makes a lot more sense if you have a better understanding of higher order components than I do
 function withStateSlice(component, slice) {
   const MemoComponent = React.memo(component);
   function Wrapper(props, ref) {
@@ -133,6 +135,24 @@ function Cell({state: cell, row, column}) {
   )
 }
 Cell = withStateSlice(Cell, (state, {row, column}) => state.grid[row][column])
+
+// function CellImpl({cell, row, column}) {
+//   const dispatch = useAppDispatch()
+//   const handleClick = () => dispatch({type: 'UPDATE_GRID_CELL', row, column})
+//   return (
+//     <button
+//       className="cell"
+//       onClick={handleClick}
+//       style={{
+//         color: cell > 50 ? 'white' : 'black',
+//         backgroundColor: `rgba(0, 0, 0, ${cell / 100})`,
+//       }}
+//     >
+//       {Math.floor(cell)}
+//     </button>
+//   )
+// }
+// CellImpl = React.memo(CellImpl)
 
 function DogNameInput() {
   const [state, dispatch] = useDogState()
